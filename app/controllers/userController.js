5e6383ad0196550a23d63947826726d233be14db
GabrielGFC <gabriel.fernandes@aluno.unievangelica.edu.br>
2023-10-23 11:46:32 -0300

    comi

--- app/controllers/userController.js
+++ app/controllers/userController.js
@@ -13,11 +13,59 @@ exports.get = async (req, res) => {
 };
 //post
 exports.register = async (req, res) => {
-  const { cpf, password, nameComplete, rg, telefone, dataNascimento, profissao,estadoCivil, sexo, email, mae, pai, paisOrigem, cep, logradouro, numero, complemento, bairro, cidade, estado } = req.body;
+  const { 
+    cpf,
+     password, 
+     nameComplete, 
+     rg, 
+     telefone, 
+     dataNascimento, 
+     profissao,estadoCivil, 
+     sexo, 
+     email, 
+     mae, 
+     pai, 
+     paisOrigem, 
+     cep, 
+     logradouro, 
+     numero, 
+     complemento, 
+     bairro, 
+     cidade, 
+     estado 
+    } = req.body;
 
   try {
       const hashedPassword = bcrypt.hashSync(password, 10);
-      await User.create({ cpf, password: hashedPassword, nameComplete, rg,telefone,dataNascimento, profissao,estadoCivil,sexo, email, mae, pai, paisOrigem, cep, logradouro, numero, complemento, bairro, cidade, estado });
+      const hashedcep = bcrypt.hashSync(cep, 10);
+      const hashedlogradouro = bcrypt.hashSync(logradouro, 10);
+      const hashednumero = bcrypt.hashSync(numero, 10);
+      const hashedcomplemento = bcrypt.hashSync(complemento, 10);
+      const hashedbairro = bcrypt.hashSync(bairro, 10);
+      const hashedcidade = bcrypt.hashSync(cidade, 10);
+      const hashedestado = bcrypt.hashSync(estado, 10);
+      await User.create({ 
+        cpf, 
+        password: hashedPassword, 
+        nameComplete, 
+        rg,
+        telefone,
+        dataNascimento, 
+        profissao,
+        estadoCivil,
+        sexo, 
+        email, 
+        mae, 
+        pai, 
+        paisOrigem, 
+        cep: hashedcep,
+        logradouro: hashedlogradouro, 
+        numero: hashednumero, 
+        complemento: hashedcomplemento, 
+        bairro: hashedbairro, 
+        cidade: hashedcidade, 
+        estado : hashedestado
+      });
       res.status(201).json({ message: 'User registered successfully' });
   } catch (error) {
       console.error(error);
@@ -28,21 +76,78 @@ exports.register = async (req, res) => {
 exports.update = async (req, res) => {
   const { user_id } = req.params;
   const { 
-     cpf, password, nameComplete, rg, telefone, 
-     dataNascimento, profissao, estadoCivil, sexo, email, mae, 
-     pai, paisOrigem, cep, logradouro, numero, 
-     complemento, bairro, cidade, estado } = req.body;
+     cpf, 
+     password, 
+     nameComplete, 
+     rg, 
+     telefone, 
+     dataNascimento, 
+     profissao, 
+     estadoCivil, 
+     sexo, 
+     email, 
+     mae, 
+     pai, 
+     paisOrigem, 
+     cep, 
+     logradouro, 
+     numero, 
+     complemento, 
+     bairro, 
+     cidade, 
+     estado 
+    } = req.body;
 
   try {
       const hashedPassword = bcrypt.hashSync(password, 10);
+      const hashedcep = bcrypt.hashSync(cep, 10);
+      const hashedlogradouro = bcrypt.hashSync(logradouro, 10);
+      const hashednumero = bcrypt.hashSync(numero, 10);
+      const hashedcomplemento = bcrypt.hashSync(complemento, 10);
+      const hashedbairro = bcrypt.hashSync(bairro, 10);
+      const hashedcidade = bcrypt.hashSync(cidade, 10);
+      const hashedestado = bcrypt.hashSync(estado, 10)
       await User.update({ 
-         cpf, password: hashedPassword, nameComplete, rg, telefone,
-         dataNascimento, profissao, estadoCivil, sexo, email, mae, pai, 
-         paisOrigem, cep, logradouro, numero, complemento, bairro, cidade, estado },
-          { where: { id: user_id } });
-      res.status(200).json({ message: 'User updated successfully' });
-  } catch (error) {
+        cpf, 
+        password: hashedPassword, 
+        nameComplete, 
+        rg,
+        telefone,
+        dataNascimento, 
+        profissao,
+        estadoCivil,
+        sexo, 
+        email, 
+        mae, 
+        pai, 
+        paisOrigem, 
+        cep: hashedcep,
+        logradouro: hashedlogradouro, 
+        numero: hashednumero, 
+        complemento: hashedcomplemento, 
+        bairro: hashedbairro, 
+        cidade: hashedcidade, 
+        estado : hashedestado
+        },
+        { where: { id: user_id } 
+  });
+    res.status(200).json({ message: 'User updated successfully' });
+      } 
+      catch (error) {
       console.error(error);
       res.status(500).json({ message: 'Internal server error' });
-  }
+    }
 }
+
+//delete
+exports.delete = async (req, res) => {
+    const { user_id } = req.params;
+    try {
+        await User.destroy({ where: { id: user_id } });
+
+        res.status(200).json({ message: 'User deleted successfully' });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Internal server error' });
+    }
+};
--- app/routes/userRoutes.js
+++ app/routes/userRoutes.js
@@ -5,5 +5,6 @@ const userController=require('../controllers/userController.js');
 router.get('/',userController.get);
 router.post('/',userController.register);
 router.put('/:user_id',userController.update);
+router.delete('/:user_id',userController.delete);
 
 module.exports = router;

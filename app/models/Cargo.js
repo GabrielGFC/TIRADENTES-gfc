de1db6a6ebfec9b17d21a6371f913310b638a9ae
Gbertonsin <gabriel_bertonsin@hotmail.com>
2023-12-15 17:46:27 -0300

    fix: :bug: Registros Iniciais sem force

--- app/models/Cargo.js
+++ app/models/Cargo.js
@@ -11,24 +11,56 @@ const Cargo = sequelize.define('cargo', {
     }
 });
 
-sequelize.sync({ force: true })
-    .then(() => {
-        return Cargo.create({
-            cargos: 'admin',
-        });
-    });
-sequelize.sync({ force: true })
-    .then(() => {
-        return Cargo.create({
-            cargos: 'aluno',
-        });
-    });
-sequelize.sync({ force: true })
-    .then(() => {
-        return Cargo.create({
-            cargos: 'colaboradores',
-        });
+// sequelize.sync({ force: true })
+//     .then(() => {
+//         return Cargo.create({
+//             cargos: 'admin',
+//         });
+//     });
+// sequelize.sync({ force: true })
+//     .then(() => {
+//         return Cargo.create({
+//             cargos: 'aluno',
+//         });
+//     });
+// sequelize.sync({ force: true })
+//     .then(() => {
+//         return Cargo.create({
+//             cargos: 'colaboradores',
+//         });
     
-    });
+//     });
+
+const registrosIniciais = async () => {
+    try {
+        // Sincroniza o banco de dados sem forçar a recriação do esquema
+        await sequelize.sync();
+
+        // Verifica se os registros já existem
+        const adminCargo = await Cargo.findOne({ where: { cargos: 'admin' } });
+        const alunoCargo = await Cargo.findOne({ where: { cargos: 'aluno' } });
+        const colaboradoresCargo = await Cargo.findOne({ where: { cargos: 'colaboradores' } });
+
+        // Cria registros se não existirem
+        if (!adminCargo) {
+            await Cargo.create({ cargos: 'admin' });
+        }
+
+        if (!alunoCargo) {
+            await Cargo.create({ cargos: 'aluno' });
+        }
+
+        if (!colaboradoresCargo) {
+            await Cargo.create({ cargos: 'colaboradores' });
+        }
+
+        console.log('Registros iniciais criados com sucesso!');
+    } catch (error) {
+        console.error('Erro ao criar registros iniciais:', error);
+    }
+};
+
+// Chama a função para criar os registros após a sincronização
+registrosIniciais();
 
 module.exports = Cargo;

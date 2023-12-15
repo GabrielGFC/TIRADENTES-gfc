7fff98bb08b072cf0f096a1247ec73a7f74cbe7d
Gbertonsin <gabriel_bertonsin@hotmail.com>
2023-12-15 19:51:24 -0300

    fix: :bug: Cargo sem force

--- app/models/Cargo.js
+++ app/models/Cargo.js
@@ -1,5 +1,7 @@
 const { Sequelize, DataTypes, STRING } = require('sequelize');
 const sequelize = require('../config/database');
+const Item = require('./Item');
+const Familia = require('./Familia');
 
 const Cargo = sequelize.define('cargo', {
 
@@ -31,36 +33,5 @@ const Cargo = sequelize.define('cargo', {
     
 //     });
 
-const registrosIniciais = async () => {
-    try {
-        // Sincroniza o banco de dados sem forçar a recriação do esquema
-        await sequelize.sync();
-
-        // Verifica se os registros já existem
-        const adminCargo = await Cargo.findOne({ where: { cargos: 'admin' } });
-        const alunoCargo = await Cargo.findOne({ where: { cargos: 'aluno' } });
-        const colaboradoresCargo = await Cargo.findOne({ where: { cargos: 'colaboradores' } });
-
-        // Cria registros se não existirem
-        if (!adminCargo) {
-            await Cargo.create({ cargos: 'admin' });
-        }
-
-        if (!alunoCargo) {
-            await Cargo.create({ cargos: 'aluno' });
-        }
-
-        if (!colaboradoresCargo) {
-            await Cargo.create({ cargos: 'colaboradores' });
-        }
-
-        console.log('Registros iniciais criados com sucesso!');
-    } catch (error) {
-        console.error('Erro ao criar registros iniciais:', error);
-    }
-};
-
-// Chama a função para criar os registros após a sincronização
-registrosIniciais();
 
 module.exports = Cargo;

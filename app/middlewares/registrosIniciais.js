e31b0f670e4e5192bbc680e58581a3e3da837e49
Gbertonsin <gabriel_bertonsin@hotmail.com>
2023-12-16 18:15:15 -0300

    feat: :sparkles: Criando Itens, caixas e estoque para teste

--- app/middlewares/registrosIniciais.js
+++ app/middlewares/registrosIniciais.js
@@ -3,6 +3,9 @@ const Cargo = require('../models/Cargo');
 const Item = require('../models/Item');
 const Familia = require('../models/Familia');
 const User = require('../models/User');
+const Caixa = require('../models/Caixa');
+const Pedido = require('../models/Pedido');
+const Estoque = require('../models/Estoque');
 const registrosIniciais = async () => {
     try {
         // Sincroniza o banco de dados sem forçar a recriação do esquema
@@ -17,6 +20,9 @@ const registrosIniciais = async () => {
         const colaboradorLogin = await User.findOne({ where: { matricula: 1234567} });
         const item = await Item.findOne({ where: { id: 1 } });
         const familia = await Familia.findOne({ where: { id: 1 } });
+        const caixa = await Caixa.findOne({ where: { idCaixa: 1 } });
+        const pedido = await Pedido.findOne({ where: { idPedido: 1 } });
+        const estoque = await Estoque.findOne({ where: { idEstoque: 1 } });
         // Cria registros se não existirem
         if (!adminCargo) {
             await Cargo.create({ cargos: 'admin' });
@@ -31,11 +37,11 @@ const registrosIniciais = async () => {
         }
 
         if (!item) {
-            await Item.create({ id: 1, nome: 'pokemon', quantidade: 2, descricao: 'oi' });
+            await Item.create({ id: 1, nome: 'Bisturi', quantidade: 2, descricao: 'O bisturi é uma ferramenta cirúrgica de lâmina afiada e pontiaguda, utilizada para realizar incisões precisas em tecidos durante procedimentos cirúrgicos' });
         }
 
         if (!familia) {
-            await Familia.create({ id: 1, nome: 'opa', descricao: 'vish'});
+            await Familia.create({ id: 1, nome: 'Cirúrgica', descricao: 'Itens usados para cirurgia'});
         }
        
         if (!adminLogin) {
@@ -48,6 +54,18 @@ const registrosIniciais = async () => {
 
         if (!colaboradorLogin) {
             await User.create({ matricula: 1234567, senha: 'colaborador', email: 'colaborador@gmail.com', nome: 'Colaborador', periodo: 2, idcargo: 3});
+        }
+
+        if (!caixa) {
+            await Caixa.create({ idItem: 1, idFamilia: 1, matricula: 1234567});
+        }
+
+        if (!pedido) {
+            await Pedido.create({ aprovacao: 1, idCaixa: 1, matricula: 7654321});
+        }
+
+        if (!estoque) {
+            await Estoque.create({ idCaixa: 1});
         }
 
         console.log('Registros iniciais criados com sucesso!');
